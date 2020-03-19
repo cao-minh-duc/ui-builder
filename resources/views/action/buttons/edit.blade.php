5b357de85d85ece0dd57cc6422ac1a66fe1aae9f
duccm <caominhduc31081@gmail.com>
2020-03-19 09:32:53 +0700

    changing edit button

--- resources/views/action/buttons/edit.blade.php
+++ resources/views/action/buttons/edit.blade.php
@@ -1,6 +1,6 @@
 
 <a 
-    class="h-full px-3 py-2 text-white bg-{{$color ?? config('ui-builder.system.color')}}-700 rounded hover:bg-{{$color ?? config('ui-builder.system.color')}}-900 shadow-md hover:shadow-none leading-9"
+    class="h-full px-3 py-2 text-white bg-{{$color ?? config('ui-builder.system.color')}}-700 rounded hover:bg-{{$color ?? config('ui-builder.system.color')}}-900 shadow-md hover:shadow-none align-middle"
     type="button" 
     dusk="edit-button" 
     href="{{ $url }}"
