68734a32e0550de3c7049d134a55d5a3f773d6c8
duccm <caominhduc31081@gmail.com>
2020-04-02 12:07:38 +0700

    update create button

--- resources/views/action/buttons/create.blade.php
+++ resources/views/action/buttons/create.blade.php
@@ -1,5 +1,5 @@
 <a 
-    class=" h-full mx-1 px-3 py-2 text-white bg-{{$color ?? config('ui-builder.system.color')}}-700 rounded hover:bg-{{$color ?? config('ui-builder.system.color')}}-900 shadow-md hover:shadow-none leading-9"
+    class="h-full mx-1 px-3 py-2 text-white bg-{{$color ?? config('ui-builder.system.color')}}-700 rounded hover:bg-{{$color ?? config('ui-builder.system.color')}}-900 shadow-md hover:shadow-none"
     type="button" 
     dusk="create-button" 
     href="{{ $url }}"
