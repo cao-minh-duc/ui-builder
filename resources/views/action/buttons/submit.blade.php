9d5f9cb84027bf25045b28f3ed33ad55bfd62491
duccm <caominhduc31081@gmail.com>
2020-04-02 21:42:06 +0700

    fix size

--- resources/views/action/buttons/submit.blade.php
+++ resources/views/action/buttons/submit.blade.php
@@ -1,4 +1,4 @@
 
-<button type="submit" class=" h-full mx-1 px-3 py-2 text-white bg-{{$color ?? config('ui-builder.system.color')}}-700 rounded hover:bg-{{$color ?? config('ui-builder.system.color')}}-900 shadow-md hover:shadow-none">
+<button type="submit" class="h-full mx-1 px-3 py-2 text-white bg-{{$color ?? config('ui-builder.system.color')}}-700 rounded hover:bg-{{$color ?? config('ui-builder.system.color')}}-900 shadow-md hover:shadow-none">
     {{$name}}
 </button>
